8769d649a8952eada5d1864941cf08cd1d6a7957
Julius Adebowale <juadebgabriel@gmail.com>
2023-12-25 01:08:51 +0100

    FEAT: Creating the latest news component

--- app/components/Hcomp/LatestNews.jsx
+++ app/components/Hcomp/LatestNews.jsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { IoChevronBackSharp, IoChevronForwardSharp } from "react-icons/io5";
+import Image from "next/image";
 
 const LatestNews = () => {
   const [newsData, setNewsData] = useState([]);
@@ -83,37 +84,80 @@ const LatestNews = () => {
       <button
         key={pageNumber}
         onClick={() => handlePageClick(pageNumber)}
-        className={currentPage === pageNumber ? "active-page" : "text-slate-400"}
+        className={
+          currentPage === pageNumber ? "active-page" : "text-slate-400"
+        }
       >
         {pageNumber}
       </button>
     ));
   };
 
+  const isBackDisabled = currentPage === 1;
+  const isNextDisabled = currentPage === totalPages;
+
+  const colors = ["border-red-500", "border-blue-500", "border-yellow-500"]; // Tailwind classes for border colors
+
+  const getRandomColorClass = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
   return (
-    <section className="h-screen px-[25px] sm:px-16 mt-10 w-full flex flex-col items-center justify-start">
-      <h1 className="text-[34px] lg:text-[44px] w-full leading-[1.2] capitalize font-[600] text-primary_red text-center">
-        Latest News & Updates
-      </h1>
-      <div className="grid grid-cols-3 gap-8">
+    <section className="h-max px-[25px] sm:px-16 mt-10 w-full flex flex-col items-center justify-start">
+      <div>
+        <h1 className="text-[34px] lg:text-[44px] w-full leading-[1.2] capitalize font-[600] text-primary_red text-center mb-3">
+          Latest News & Updates
+        </h1>
+        <p className="text-[19px] text-center leading-normal mb-10">
+          Lorem ipsum dolor sit amet consectetur. Euismod amet in mi integer
+          risus leo enim mi. Neque viverra facilisi nec semper nullam ipsum. In
+          imperdiet cursus augue donec elit nibh.
+        </p>
+      </div>
+      <div className="grid grid-cols-3 gap-8 mb-10">
         {newsData.map((newsItem) => (
-          <div key={newsItem.id}>
-            <h2>{newsItem.events_news_name}</h2>
-            {/* Render other details */}
+          <div key={newsItem.id} className="shadow-md p-4 rounded-md">
+            <div>
+              <img
+                alt="page image"
+                src={newsItem.events_news_front_image}
+                className="w-full max-h-[300px]"
+              />
+            </div>
+            <div>
+              <span
+                className={`inline-block px-2 py-0 text-[#232323]/30 tracking-wider uppercase text-[12px] border-s-2 ${getRandomColorClass()}`}
+              >
+                News
+              </span>
+
+              <h2>{newsItem.events_news_name}</h2>
+            </div>
           </div>
         ))}
       </div>
       <div className="self-end flex items-center justify-center space-x-3">
         <button
-          className="flex items-center space-x-1 justify-center"
+          className={
+            isBackDisabled
+              ? "flex items-center space-x-1 justify-center opacity-50"
+              : "flex items-center space-x-1 justify-center opacity-100"
+          }
           onClick={goToPreviousPage}
           disabled={currentPage === 1}
         >
           <IoChevronBackSharp /> <span>Back</span>
         </button>
-        <span className="flex item-center justify-center space-x-3">{generatePagination()}</span>
+        <span className="flex item-center justify-center space-x-3">
+          {generatePagination()}
+        </span>
         <button
-          className="flex items-center space-x-1 justify-center"
+          className={
+            isNextDisabled
+              ? "flex items-center space-x-1 justify-center opacity-50"
+              : "flex items-center space-x-1 justify-center opacity-100"
+          }
           onClick={goToNextPage}
           disabled={currentPage === totalPages}
         >
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["www.sicklecellfoundation.com"],
+  },
+};
 
 module.exports = nextConfig;
