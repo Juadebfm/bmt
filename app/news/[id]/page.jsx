ff88a6ecac495924d93378fdfe658868a75e7bb0
Julius Adebowale <juadebgabriel@gmail.com>
2023-12-29 17:41:50 +0100

    FIX: Setting up the news details page

--- app/news/[id]/page.jsx
+++ app/news/[id]/page.jsx
@@ -9,7 +9,7 @@ const NewsDetails = ({ params }) => {
     const storedNewsData = localStorage.getItem("newsData");
     if (storedNewsData) {
       const parsedNewsData = JSON.parse(storedNewsData);
-      const parsedId = parseInt(params.id); // Parse params.id to an integer
+      const parsedId = parseInt(params.id, 10); // Parse params.id to an integer
 
       const newsItem = parsedNewsData.find((item) => item.id === parsedId);
       if (newsItem) {
@@ -24,33 +24,28 @@ const NewsDetails = ({ params }) => {
   }, [params.id]);
 
   return (
-    <section className="px-16 py-10">
+    <section className="px-[25px] md:px-16 py-[20px] md:py-10">
       <div className="flex items-center justify-between py-4">
-        <button className="flex items-center justify-between">
-          <IoChevronBackSharp className="text-3xl" />
-          <span>Back</span>
-        </button>
-        <button className="flex items-center justify-between">
-          <span>Next</span>
-          <IoChevronForwardSharp className="text-3xl" />
-        </button>
-      </div>
-      <div className="flex flex-wrap justify-start mt-8">
-        {newsDetails.event_news_image_array.map((image, index) => (
-          <img
-            key={index}
-            src={image.file_url}
-            alt={`Image ${index + 1}`}
-            className="w-40 h-40 object-cover m-2"
-          />
-        ))}
+        {/* Back and Next buttons */}
       </div>
       {newsDetails ? (
         <div>
-          <h2 className="text-[32px] font-bold leading-tight mb-5">
+          <h2 className="text-[22px] md:text-[32px] font-bold leading-tight mb-5">
             {newsDetails.events_news_name}
           </h2>
-          <p>{newsDetails.events_news_content}</p>
+          <div className="flex overflow-x-auto scroll_snap scrollbar-thin scrollbar-thumb-primary_dark scrollbar-track-primary_faded_grey/50 justify-start gap-5 mt-8 mb-10">
+            {newsDetails.event_news_image_array.map((image, index) => (
+              <img
+                key={index}
+                src={image.file_url}
+                alt={`Image ${index + 1}`}
+                className="w-40 h-40 object-cover m-2 h-[260px] w-[350px] rounded-2xl shadow-md border border-slate-100"
+              />
+            ))}
+          </div>
+          <div>
+            <p>{newsDetails.events_news_content}</p>
+          </div>
         </div>
       ) : (
         <p>Loading...</p>
