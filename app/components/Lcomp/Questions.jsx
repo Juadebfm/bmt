7ebd518113b53f289a92366c7717a8109ccca854
Julius Adebowale <juadebgabriel@gmail.com>
2023-12-29 20:11:37 +0100

    FEAT: Completed the FAQ questions sections

--- app/components/Lcomp/Questions.jsx
+++ app/components/Lcomp/Questions.jsx
@@ -60,59 +60,59 @@ const Questions = () => {
     <section className="space-y-3 mb-20">
       <FAQItem
         question="What is Bone Marrow Transplant (BMT)?"
-        answer="A BMT is a medical procedure that involves replacing or restoring damaged or destroyed bone marrow with healthy stem cells, essential for blood cell production."
+        answer="A BMT is a medical procedure that involves replacing or restoring damaged or destroyed bone marrow with healthy stem cells, essential for blood cell production"
       />
       <FAQItem
         question="What are Indications for BMT?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="BMT is undertaken to treat various haematological disorders, such as leukaemia, lymphoma, sickle cell, myeloma, aplastic anaemia, and certain genetic conditions, by rejuvenating blood-forming cells"
       />
       <FAQItem
         question="What Diseases Can be Cured with BMT?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="BMT is effective in treating a range of diseases, including leukaemia, lymphoma, myeloma, aplastic anaemia, and certain genetic disorders affecting the blood."
       />
       <FAQItem
         question="What are the common Complications of BMT?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="Risks include infections, graft failure, graft-versus-host disease (GVHD), and potential side effects from high-dose chemotherapy or radiation during the conditioning process."
       />
       <FAQItem
         question="What is Graft-Versus-Host Disease (GVHD)?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="GVHD is a complication where donor cells attack the recipient's tissues. Preventive measures and treatments are implemented to minimize its occurrence and impact."
       />
       <FAQItem
         question="How Can I Prepare for a BMT?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="Preparation involves consultation with the transplant team, eligibility assessments, stem cell collection (autologous or allogeneic), and the conditioning process involving chemotherapy or radiation."
       />
       <FAQItem
         question="What Happens During the BMT Procedure?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="The procedure includes the infusion of healthy stem cells into the patient's bloodstream. The conditioning process eliminates diseased cells and creates an environment for the engraftment of new cells."
       />
       <FAQItem
         question="What Hospital Processes Should I Expect?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="Hospital processes involve continuous monitoring, infection prevention measures, managing potential complications, and a focus on supportive care during the recovery phase."
       />
       <FAQItem
         question="What Medications Will I Take After BMT?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="Post-BMT medications may include immunosuppressants, anti-infective medications, and symptom management drugs. Adherence to prescribed medications is crucial for ongoing well-being."
       />
       <FAQItem
         question="How Long Does the Recovery Process Take?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="The recovery timeline varies, but patients typically transition to outpatient care as they progress. Continuous monitoring and follow-ups are part of the ongoing recovery process."
       />
       <FAQItem
         question="What Lifestyle Adjustments Are Necessary After BMT?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="Lifestyle adjustments may include nutritional guidance, exercise, stress management, and maintaining a healthy routine to support overall well-being."
       />
       <FAQItem
         question="How Can I Cope with the Emotional Challenges Post-BMT?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="Coping strategies involve accessing counselling services, participating in support groups, engaging in creative therapies, and celebrating milestones to foster emotional resilience."
       />
       <FAQItem
         question="Can I Return to Normal Activities After BMT?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="Gradual reintegration into daily life activities is encouraged. This may include work, social events, and other routines, guided by your healthcare team's recommendations."
       />
       <FAQItem
         question="What Ongoing Support is Available?"
-        answer="During a bone marrow transplant, unhealthy marrow is replaced by healthy cells. These cells can come from a donor or can be harvested from the patient's own body."
+        answer="Ongoing support includes regular check-ins with the healthcare team, educational resources, and involvement in a supportive community through support groups and peer mentoring programs."
       />
     </section>
   );
